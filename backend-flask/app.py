ba197ac871c9e642e92693ddce2f5927170a41fb
oliverbebber <67529552+oliverbebber@users.noreply.github.com>
2023-02-21 18:13:43 -0500

    create new notifications backend endpoint

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -61,14 +61,14 @@ def data_create_message():
     return model['data'], 200
   return
 
-@app.route("/api/activities/notifications", methods=['GET'])
-def data_home():
-  data = NotificationsActivities.run()
-  return data, 200
-
 @app.route("/api/activities/home", methods=['GET'])
 def data_home():
   data = HomeActivities.run()
+  return data, 200
+
+@app.route("/api/activities/notifications", methods=['GET'])
+def data_notifications():
+  data = NotificationsActivities.run()
   return data, 200
 
 @app.route("/api/activities/@<string:handle>", methods=['GET'])
--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -4,8 +4,8 @@ class HomeActivities:
     now = datetime.now(timezone.utc).astimezone()
     results = [{
       'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
-      'handle':  'Andrew Brown',
-      'message': 'Cloud is fun!',
+      'handle':  'andrew brown',
+      'message': 'cloud is fun!',
       'created_at': (now - timedelta(days=2)).isoformat(),
       'expires_at': (now + timedelta(days=5)).isoformat(),
       'likes_count': 5,
@@ -14,7 +14,7 @@ class HomeActivities:
       'replies': [{
         'uuid': '26e12864-1c26-5c3a-9658-97a10f8fea67',
         'reply_to_activity_uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
-        'handle':  'Worf',
+        'handle':  'worf',
         'message': 'This post has no honor!',
         'likes_count': 0,
         'replies_count': 0,
@@ -24,7 +24,7 @@ class HomeActivities:
     },
     {
       'uuid': '66e12864-8c26-4c3a-9658-95a10f8fea67',
-      'handle':  'Worf',
+      'handle':  'worf',
       'message': 'I am out of prune juice',
       'created_at': (now - timedelta(days=7)).isoformat(),
       'expires_at': (now + timedelta(days=9)).isoformat(),
@@ -33,8 +33,8 @@ class HomeActivities:
     },
     {
       'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
-      'handle':  'Garek',
-      'message': 'My dear doctor, I am just simple tailor',
+      'handle':  'garek',
+      'message': 'my dear doctor, i am just simple tailor',
       'created_at': (now - timedelta(hours=1)).isoformat(),
       'expires_at': (now + timedelta(hours=12)).isoformat(),
       'likes': 0,
