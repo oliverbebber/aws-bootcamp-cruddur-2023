f9107a0ebe38155f6c1f5521f69f96efc6554198
oliverbebber <67529552+oliverbebber@users.noreply.github.com>
2023-02-27 12:09:51 -0500

    Add ConsoleSpanExporter + SimpleSpanProcessor

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -21,6 +21,7 @@ from opentelemetry.instrumentation.requests import RequestsInstrumentor
 from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProcessor
 
 # HoneyComb Updates -------
 # Initialize tracing and an exporter that can send data to Honeycomb
@@ -28,6 +29,11 @@ provider = TracerProvider()
 # OTLPSpanExporter reads the Env Vars for configuring the location to send spans to
 processor = BatchSpanProcessor(OTLPSpanExporter())
 provider.add_span_processor(processor)
+
+# Show this in logs within backend-flask app (STDOUT)
+simple_processor = SimpleSpanProcessor(ConsoleSpanExporter())
+provider.add_simple_span_processor(simple_processor)
+
 trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
